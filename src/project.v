7c82e2ffd115161fc5a9afdeb04a4cfbc7ee8403
luisalvarez362 <luis.alvarez362@uppuebla.edu.mx>
2026-04-29 16:42:03 -0600

    Update project.v

--- src/project.v
+++ src/project.v
@@ -29,5 +29,5 @@ wire rst;
    // assign rst=!rst_n;
 
     //henon_map #(.WIDTH(8),.FRAC(4)) henon (.clk(clk),.rst(rst),.en(ui_in[0]),.x_out(uo_out),.y_out(uio_out),.valid(valid));
-    top u1 (.clk(clk),.rst_n(rst_n),.ena_henon(ui_in[0]), .ena_ser_x(ui_in[1]),.ena_ser_y(ui_in[2]),.Q_ser_x(uo_out[0]),.eos_x(uo_out[1]),.Qser_y(uo_out[2]),.eos_y(uo_out[3]),.done_henon(uo_out[4]));
+    top_henon u1(.clk(clk),.rst_n(rst_n),.ena_henon(ui_in[0]), .ena_ser_x(ui_in[1]),.ena_ser_y(ui_in[2]),.Q_ser_x(uo_out[0]),.eos_x(uo_out[1]),.Qser_y(uo_out[2]),.eos_y(uo_out[3]),.done_henon(uo_out[4]));
 endmodule
