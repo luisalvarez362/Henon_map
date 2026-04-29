b3de9ee8584c05cbb735f54c3e58327b4718aebb
luisalvarez362 <luis.alvarez362@uppuebla.edu.mx>
2026-04-29 15:50:33 -0600

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_henon_map (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,12 +17,12 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  //assign uio_out = 0;
+  assign uio_oe  = 8'b11111111;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ui_in[1], ui_in[2], ui_in[3],ui_in[4],ui_in[5],ui_in[6],ui_in[7], 1'b0};
 wire rst;
     wire valid;
     assign rst=!rst_n;
